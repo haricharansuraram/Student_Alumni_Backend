98f45c70595a6e2fd357b91c07eae66050663348
Hari <haricharansuraram@gmail.com>
2025-08-13 12:26:28 +0530

    Initial commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,23 @@
 const asyncHandler = require('express-async-handler');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const Profile = require('../models/Profile'); // NEW: Import the Profile model
-const Alumni = require('../models/Alumni'); // NEW: Import the Alumni model
+const Profile = require('../models/Profile');
+const Alumni = require('../models/Alumni');
 
-// @desc    Register a new user
-// @route   POST /api/users/register
-// @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, role } = req.body;
 
-  // Basic validation
   if (!name || !email || !password || !role) {
     res.status(400);
     throw new Error('Please enter all fields');
   }
 
-  // Check if user exists
   const userExists = await User.findOne({ email });
   if (userExists) {
     res.status(400);
     throw new Error('User already exists');
   }
 
-  // Create user
   const user = await User.create({
     name,
     email,
@@ -32,10 +26,8 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (user) {
-    // NEW: Automatically create a blank profile for the new user
     await Profile.create({ user: user._id });
 
-    // If the role is Alumni, create an Alumni profile
     if (user.role.toLowerCase() === 'alumni') {
         const { batch, profession, location } = req.body;
         await Alumni.create({
@@ -43,7 +35,7 @@ const registerUser = asyncHandler(async (req, res) => {
             jobTitle: profession,
             location,
             batch,
-            industry: 'N/A' // Default value
+            industry: 'N/A'
         });
     }
 
@@ -60,13 +52,9 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc    Authenticate a user
-// @route   POST /api/users/login
-// @access  Public
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  // Check for user email
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
@@ -83,7 +71,6 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
-// Generate JWT
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: '30d',
