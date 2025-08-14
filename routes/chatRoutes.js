af7af478246a900b2998d6591651338d06f414e9
Hari <haricharansuraram@gmail.com>
2025-08-14 23:49:28 +0530

    Msg and ALumin search

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,10 +1,10 @@
-// routes/chatRoutes.js
 const express = require('express');
 const router = express.Router();
-const { getConversations, sendMessage } = require('../controllers/chatController');
+const { getConversations, createOrGetChat, sendMessage } = require('../controllers/chatController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.get('/', protect, getConversations);
+router.post('/', protect, createOrGetChat);
 router.post('/:chatId/messages', protect, sendMessage);
 
 module.exports = router;
